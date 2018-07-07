5d90b2f4b25b5705577c131d116325163990ef32
jitendershahi <jitendersharmasdec@gamil.com>
2018-07-07 12:25:44 +0530

    feature:create app setup with angular 6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { BaseModule } from '@app/shared/base.module';
+import { appRoutes } from '@app/app.routing';
+import { AppComponent } from '@app/app.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    BaseModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
